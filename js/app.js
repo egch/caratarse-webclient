55482d8a6a23249377667b7593cc4130b55330c5
egch <enricogiurin@gmail.com>
2015-06-18 04:08:21 +0200

    draft implementation of delete user

--- js/app.js
+++ js/app.js
@@ -1,33 +1,28 @@
 var caratarseApp = angular.module('caratarse', ['ngRoute', 'ngResource']);
 
-caratarseApp.factory("userService", function ($resource) {
-    return $resource('http://localhost:8081/users.json',
-        {'query': { method: 'GET' }});
+
+caratarseApp.factory('User', function($resource){
+    return  $resource('http://localhost:8081/users/:uuid')
+
+});
+
+caratarseApp.controller('ListUsersController',
+    function ($scope, User) {
+        User.get(function(data) {
+            $scope.data = data;
+        });
 });
 
 
-/*caratarseApp.controller('ListController', ['$scope', '$http',
-    function ($scope, $http) {
-
-        $http.get('data/users.json').success(function (data) {
-            $scope.data = data;
-        });
-    }]);*/
-
-caratarseApp.controller('ListController',
-    function ($scope, userService) {
-        userService.get(function(data) {
-            $scope.data = data;
-        });
-
-
+caratarseApp.controller('DeleteUserController',
+    function ($scope, User, $routeParams) {
+        User.delete({uuid:$routeParams.uuid});
 
 });
 
 
 
-
-
+/* ********** ngRoute section ********** */
 
 caratarseApp.config(['$routeProvider',
     function ($routeProvider) {
@@ -36,8 +31,13 @@ caratarseApp.config(['$routeProvider',
                 templateUrl: 'views/home.html'
             }).
             when('/users', {
-                templateUrl: 'views/listUsers.html',
-                controller: 'ListController'
+                controller:  'ListUsersController',
+                templateUrl: 'views/listUsers.html'
+            }).
+            when('/deleteUser/:uuid', {
+                controller:  'DeleteUserController',
+                templateUrl: 'views/home.html'
+
             }).
             otherwise({
                 redirectTo: '/'
